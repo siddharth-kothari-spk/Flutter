fd1d395835a1715293109cb391655802e334a42e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-10 12:47:54 +0530

    adding image from url to body

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -5,10 +5,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar:
-            AppBar(title: Text('I Am Rich'), backgroundColor: Colors.redAccent),
-        backgroundColor: Colors.lime,
-      ),
+          appBar: AppBar(
+              title: Text('I Am Rich'), backgroundColor: Colors.redAccent),
+          backgroundColor: Colors.lime,
+          body: const Center(
+            child: Image(
+              image: NetworkImage('https://picsum.photos/300/300'),
+            ),
+          )),
     ),
   );
 }
