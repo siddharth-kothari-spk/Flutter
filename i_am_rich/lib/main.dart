a05f9d0d1aa1047fc65dc6261fe7ed61b5584e9e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-10 11:59:06 +0530

    placing text inside center widget

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hello  Kothari")));
+  runApp(const MaterialApp(home: Center(child: Text('Hello  Kothari'))));
 }
