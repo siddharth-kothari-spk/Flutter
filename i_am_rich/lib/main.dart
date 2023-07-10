d5005b7777f2285b86951abb26b0706037b5a023
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-10 14:47:10 +0530

    incorporating all assets from a folder

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       home: Scaffold(
           appBar: AppBar(
               title: Text('I Am Rich'), backgroundColor: Colors.redAccent),
-          backgroundColor: Colors.lime,
+          backgroundColor: Colors.blueGrey,
           body: const Center(
             child: Image(
               image: AssetImage('images/diamond.png'),
