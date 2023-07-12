753776c4f7f0c79ec46bc1ee960bad7c4daf6f6a
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-12 17:46:34 +0530

    module 3 challenge

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -13,49 +13,38 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.tealAccent,
         body: SafeArea(
-          child: Column(
-            verticalDirection: VerticalDirection.down,
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 color: Colors.redAccent,
                 child: Text("column child1"),
                 height: 100,
                 width: 100,
-                margin: EdgeInsets.fromLTRB(
-                    10, 10, 10, 10), // offset from edges of screen,
                 padding: EdgeInsets.only(left: 10, top: 10),
               ),
-              Container(
-                color: Colors.blueAccent,
-                child: Text("column child2"),
-                height: 100,
-                width: 100,
-                margin: EdgeInsets.fromLTRB(
-                    10, 10, 10, 10), // offset from edges of screen,
-                padding: EdgeInsets.only(left: 10, top: 10),
-              ),
-              Row(children: [
+              Column(mainAxisAlignment: MainAxisAlignment.center, children: [
                 Container(
                   color: Colors.greenAccent,
                   child: Text("row child1"),
                   height: 100,
                   width: 100,
-                  margin: EdgeInsets.fromLTRB(
-                      10, 10, 10, 10), // offset from edges of screen,
-                  padding: EdgeInsets.only(left: 10, top: 10),
                 ),
                 Container(
                   color: Colors.amberAccent,
                   child: Text("row child2"),
                   height: 100,
                   width: 100,
-                  margin: EdgeInsets.fromLTRB(
-                      10, 10, 10, 10), // offset from edges of screen,
-                  padding: EdgeInsets.only(left: 10, top: 10),
                 ),
-              ])
+              ]),
+              Container(
+                color: Colors.blueAccent,
+                child: Text("column child2"),
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.only(left: 10, top: 10),
+              ),
             ],
           ),
         ),
